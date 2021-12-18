9befcd916df2f14e4e63e205f9da96b065f94285
nfaridHU <35460746+nfaridHU@users.noreply.github.com>
2021-12-18 22:23:44 +0500

    First Commit for SO

---
+++ SO.py
@@ -0,0 +1,47 @@
+class Vehicle:
+
+	def __init__(self):
+		pass
+
+	def get_mileage(self):
+		pass
+
+	def save_to_db(self):
+		pass
+
+	def drive(self):
+		pass
+
+	def alert_emergency(self):
+		pass
+
+
+
+# following `S` and `O`
+
+
+class Vehicle:
+	def __init__(self):
+		pass
+
+	def get_mileage(self):
+		pass
+
+	def drive(self):
+		pass
+
+
+def SmartVehicle(Vehicle):
+	def __init__(self):
+		pass
+
+	def alert_emergency(self):
+		pass
+
+
+class VehicleDB:
+	def __init__(self):
+		pass
+
+	def save_to_db(self):
+		pass
